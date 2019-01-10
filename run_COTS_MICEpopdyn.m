3f9c3f8e435625fbc98e62f2351229ef80ff00f0
khock1 <45620184+khock1@users.noreply.github.com>
2019-01-10 14:09:18 +1000

    Update run_COTS_MICEpopdyn.m

--- run_COTS_MICEpopdyn.m
+++ run_COTS_MICEpopdyn.m
@@ -1,17 +1,16 @@
 % Implementation of a published MICE model by Morello et al. 2014, MEPS
 % Karlo Hock, University of Queensland, v1 2014; v2 2019
 
-%pick any number of years
-years=18;
+% any number of years
+years = 18;
 
-% runs an example scenario of the COTS-coral population dynamics model published 
-% by Morello et al 2014, MEPS;
+% runs an example scenario of the COTS-coral population dynamics model published by Morello et al 2014, MEPS;
 % See COTS_MICE_popdyn file for more details;
 % cots_numbers returns all three size categories from the model
-[cots_numbers] = COTS_MICE_popdyn( years  );
+[cots_numbers] = COTS_MICE_popdyn(years);
 
-% plot the adult popualtion over time
-plot(0:18,cots_numbers(:,3));
+% plot the adult population over time
+plot(0:18, cots_numbers(:,3));
 title('Adult COTS abundance', 'FontSize', 11);
 xlabel('Years');
 ylabel('COTS per tow');
